c442a79b2fad58b581153fc00bc2ee849f0ea79a
davodhiambo <jesseomolo@yahoo.com>
2024-11-18 12:45:01 +0300

    update

--- greeting/greet.go
+++ greeting/greet.go
@@ -4,10 +4,5 @@ import "fmt"
 
 func Greeting() {
 	fmt.Println("Welcome to Your To-Do List\nPlease Pick an Option")
-	fmt.Println("Key '1' to Add Task")
-	fmt.Println("Key '2' to View Task")
-	fmt.Println("Key '3' to Delete Task")
-	fmt.Println("Key '4' to Update Task")
-	fmt.Println("Key '5' to Exit Program")
 	fmt.Println()
 }
--- main.go
+++ main.go
@@ -18,15 +18,25 @@ func main() {
 
 	greeting.Greeting()
 	for scanner.Scan() {
+		fmt.Println("Key '1' to Add Task")
+		fmt.Println("Key '2' to View Task")
+		fmt.Println("Key '3' to Delete Task")
+		fmt.Println("Key '4' to Update Task")
+		fmt.Println("Key '5' to Exit Program")
+		fmt.Println()
+		
 		if scanner.Text() == "1" {
+			// Prompt user to add a description for new task
 			add.PromptDescription()
 			scanner.Scan()
 			task = add.AddDescription(scanner.Text(), task)
 
+			// Prompt user to add a priority for new task
 			add.AddPriority()
 			scanner.Scan()
 			task, err = add.CheckPriority(scanner.Text(), task)
-
+		
+			// Handle errors when adding priority
 			for err != nil {
 				fmt.Println(err)
 				add.AddPriority()
@@ -34,6 +44,9 @@ func main() {
 				task, err = add.CheckPriority(scanner.Text(), task)
 			}
 			tasks = add.Append(task, tasks)
+			fmt.Println("Task added successfully!")
+			fmt.Println()
+			continue
 		} else if scanner.Text() == "2" {
 			fmt.Println("Your Tasks:")
 			for _, task := range tasks {
