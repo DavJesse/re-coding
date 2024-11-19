c7cc64d17433b99aa852104efca0533aeaaf8470
davodhiambo <jesseomolo@yahoo.com>
2024-11-19 16:12:16 +0300

    feat: add DeleteTask.

---
+++ delete/delete.go
@@ -0,0 +1,14 @@
+package delete
+
+import "davodhiambo/models"
+
+func DeleteTask(id int, tasks []models.Task) []models.Task {
+	var result []models.Task
+
+	for _, task := range tasks {
+		if task.ID != id {
+			result = append(result, task)
+		}
+	}
+	return result
+}
