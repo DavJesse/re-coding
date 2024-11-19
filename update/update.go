6b12922dfbd5cd6b440ddec9282cbde9bdfef6d1
davodhiambo <jesseomolo@yahoo.com>
2024-11-19 17:25:58 +0300

    feat: add UpdateTask.

---
+++ update/update.go
@@ -0,0 +1,15 @@
+package update
+
+func UpdateTask(id int, description string, priority int, tasks []models.Task) []models.Task {
+	for _, task := range tasks {
+		if task.ID == id {
+			if description != "" {
+				task.Description = description
+			}
+			if priority > 0 && priority <= 3 {
+                task.Priority = priority
+            }
+		}
+	}
+	return tasks
+}
